d5d5aefe595668a84bb5444a222d8fa1ea84f64e
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-01-31 18:56:48 -0500

    add dynamic components

--- src/app/page1.component.ts
+++ src/app/page1.component.ts
@@ -1,13 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, Injector, ViewChild, ViewContainerRef, OnInit } from "@angular/core";
 import { Subject } from "rxjs";
+import { ComponentFactoryResolver, ComponentRef } from '@angular/core';
+import { DynamicComponentComponent } from './dynamic-component.component';
 
 @Component({
   templateUrl: "./page1.component.html",
   styleUrls: ["./page1.component.css"],
   selector: "app-page1"
 })
-export class Page1Component { 
+export class Page1Component implements OnInit { 
 
+  constructor(
+    private readonly _componentFactoryResolver: ComponentFactoryResolver,
+    private readonly _injector: Injector
+
+  ) { }
+
+  ngOnInit() {
+    let componentFactory: any = this._componentFactoryResolver.resolveComponentFactory(DynamicComponentComponent);
+    const ref: any = this.target.createComponent(<any>componentFactory, null, this._injector);
+  }
+
+  @ViewChild("target", { read: ViewContainerRef })
+  target: ViewContainerRef;
+  
   public onDestroy: Subject<void> = new Subject<void>();
 
   ngOnDestroy() {
